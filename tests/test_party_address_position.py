80fd4009773ef64f5c1b51ba61566ee1b9d65d8f
resteve <resteve@zikzakmedia.com>
2015-12-01 11:11:04 +0100

    Tests

--- tests/test_party_address_position.py
+++ tests/test_party_address_position.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the party_address_position module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyAddressPositionTestCase(unittest.TestCase):
+class PartyAddressPositionTestCase(ModuleTestCase):
     'Test Party Address Position module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_address_position')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_address_position')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'party_address_position'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PartyAddressPositionTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
